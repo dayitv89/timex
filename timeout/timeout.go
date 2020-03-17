5c0b9a90940294c54f0ad2dba22e54681b108477
Gaurav Sharma <gaurav.sharma@punchh.com>
2020-03-17 16:07:16 +0530

    89.2% coverage, WIP test remaining

--- timeout/timeout.go
+++ timeout/timeout.go
@@ -2,7 +2,6 @@ package timeout
 
 import (
 	"context"
-	"fmt"
 	"time"
 )
 
@@ -25,35 +24,45 @@ const (
 
 //Manager where every thing going to manage
 type Manager struct {
-	handler   Handler
-	limit     int
-	duration  time.Duration
-	from      FromItem
-	ctx       context.Context
-	ctxCancel context.CancelFunc
-	wip       bool
-	buffer    []interface{}
-	wipBuffer []interface{}
+	handler          Handler
+	limit            int
+	duration         time.Duration
+	from             FromItem
+	ctx              context.Context
+	ctxCancel        context.CancelFunc
+	clean            chan bool
+	discardRemaining bool
+	wip              bool
+	buffer           []interface{}
+	wipBuffer        []interface{}
 }
 
 //NewManager create new instance
 func NewManager(h Handler, l int, d time.Duration, f FromItem) *Manager {
 	m := &Manager{
-		handler:   h,
-		limit:     l,
-		duration:  d,
-		from:      f,
-		wip:       false,
-		buffer:    emptyInterfaceArray(),
-		wipBuffer: emptyInterfaceArray(),
+		handler:          h,
+		limit:            l,
+		duration:         d,
+		from:             f,
+		clean:            make(chan bool),
+		discardRemaining: false,
+		wip:              false,
+		buffer:           emptyInterfaceArray(),
+		wipBuffer:        emptyInterfaceArray(),
 	}
 	m.setContextAndStart()
 	return m
 }
 
 //Close the timer and all running task gracefully
-func (m *Manager) Close() error {
-	return fmt.Errorf("TODO: write closing statements")
+func (m *Manager) Close() {
+	m.ctxCancel()
+}
+
+//CloseAndDiscardRemaining the timer and discard remaining buffer data
+func (m *Manager) CloseAndDiscardRemaining() {
+	m.discardRemaining = true
+	m.Close()
 }
 
 //Append object where you call this as thread safe
@@ -65,8 +74,7 @@ func (m *Manager) Append(i ...interface{}) {
 			} else {
 				m.buffer = append(m.buffer, i...)
 				if m.from == LastItem {
-					m.ctxCancel()
-					m.setContextAndStart()
+					m.TimerRestart()
 				}
 				if len(m.buffer) >= m.limit {
 					m.process()
@@ -78,8 +86,14 @@ func (m *Manager) Append(i ...interface{}) {
 
 func (m *Manager) startTimer() {
 	go func(m *Manager) {
-		<-m.ctx.Done()
-		m.process()
+		select {
+		case <-m.ctx.Done():
+			if !m.discardRemaining {
+				m.process()
+			}
+		case <-m.clean:
+			return
+		}
 	}(m)
 }
 
@@ -114,12 +128,13 @@ func (m *Manager) setContextAndStart() {
 	m.startTimer()
 }
 
-//TimerForceStop call when you want to invalidate the current timer, restart when you call TimerForceRestart.
-func (m *Manager) TimerForceStop() error {
-	return fmt.Errorf("TODO: write code for this")
+//TimerStop call when you want to invalidate the current timer, restart when you call TimerForceRestart.
+func (m *Manager) TimerStop() {
+	m.clean <- true
 }
 
-//TimerForceRestart re-initiate timer and discard current timer.
-func (m *Manager) TimerForceRestart() error {
-	return fmt.Errorf("TODO: write code for this")
+//TimerRestart re-initiate timer and discard current timer.
+func (m *Manager) TimerRestart() {
+	m.TimerStop()
+	m.setContextAndStart()
 }
--- timeout/timeout_test.go
+++ timeout/timeout_test.go
@@ -6,11 +6,12 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 )
 
 type testHandler struct {
-	data []string
+	totalData   []string
+	currentData []string
+	count       int
 }
 
 func (h *testHandler) ValidateBeforeAdd(d interface{}) bool {
@@ -18,10 +19,13 @@ func (h *testHandler) ValidateBeforeAdd(d interface{}) bool {
 }
 
 func (h *testHandler) Process(d ...interface{}) error {
+	h.count++
+	h.currentData = []string{}
 	for _, d1 := range d {
 		s, ok := d1.(string)
 		if ok {
-			h.data = append(h.data, s)
+			h.totalData = append(h.totalData, s)
+			h.currentData = append(h.currentData, s)
 		}
 	}
 	return nil
@@ -31,50 +35,163 @@ func (h *testHandler) HandleProcessingError(e error) {
 	fmt.Println("some error during processing")
 }
 
-func TestTimeout(t *testing.T) {
-	suite.Run(t, new(TimeoutSuite))
+//First Items
+func TestItemLimitFullFirstItem(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 10, 10*time.Second, FirstItem)
+	defer manager.Close()
+
+	for i := 0; i < 12; i++ {
+		manager.Append(fmt.Sprintf("a_%d", i+1))
+	}
+
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
 }
 
-type TimeoutSuite struct {
-	suite.Suite
-}
-
-func (suite *TimeoutSuite) SetupSuite() {
-	fmt.Println("before all")
-}
-
-func (suite *TimeoutSuite) SetupTest() {
-	fmt.Println("before each")
-}
-
-func (suite *TimeoutSuite) TearDownTest() {
-	fmt.Println("after each")
-}
-
-func (suite *TimeoutSuite) TearDownSuite() {
-	fmt.Println("after all")
-}
-
-func (suite *TimeoutSuite) TestLimitFullFirstItem() {
+func TestItemLimitFullFirstItemWithClose(t *testing.T) {
 	h := new(testHandler)
 	manager := NewManager(h, 10, 10*time.Second, FirstItem)
 
 	for i := 0; i < 12; i++ {
-		manager.Append(fmt.Sprintf("a_%d", i))
+		manager.Append(fmt.Sprintf("a_%d", i+1))
 	}
 
-	assert.Equal(suite.T(), 10, len(h.data), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.data)))
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
 
+	manager.Close()
+	time.Sleep(500 * time.Microsecond)
+	assert.Equal(t, 2, h.count, fmt.Sprintf("Test failed process called more than expected 2 != %d", h.count))
+	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
+	assert.Equal(t, 2, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 2 != %d", len(h.currentData)))
+}
+func TestItemLimitFullFirstItemWithCloseDiscard(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 10, 10*time.Second, FirstItem)
+
+	for i := 0; i < 12; i++ {
+		manager.Append(fmt.Sprintf("a_%d", i+1))
+	}
+
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+
+	manager.CloseAndDiscardRemaining()
+	time.Sleep(500 * time.Microsecond)
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
 }
 
-func (suite *TimeoutSuite) TestLimitFullLastItem() {
+func TestTimeLimitFullFirstItem(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 20, 1*time.Second, FirstItem)
+	defer manager.Close()
+
+	for i := 0; i < 12; i++ {
+		manager.Append(fmt.Sprintf("a_%d", i+1))
+		time.Sleep(100 * time.Millisecond)
+	}
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+}
+
+func TestTimeLimitFullFirstItemWithClose(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 20, 1*time.Second, FirstItem)
+
+	for i := 0; i < 12; i++ {
+		manager.Append(fmt.Sprintf("a_%d", i+1))
+		time.Sleep(100 * time.Millisecond)
+	}
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+
+	manager.Close()
+	time.Sleep(500 * time.Microsecond)
+	assert.Equal(t, 2, h.count, fmt.Sprintf("Test failed process called more than expected 2 != %d", h.count))
+	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
+	assert.Equal(t, 2, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 2 != %d", len(h.currentData)))
+}
+
+// Last Items
+func TestItemLimitFullLastItem(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 10, 10*time.Second, LastItem)
+	defer manager.Close()
+
+	for i := 0; i < 12; i++ {
+		item := fmt.Sprintf("a_%d", i+1)
+		manager.Append(item)
+	}
+
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+}
+
+func TestItemLimitFullLastItemWithClose(t *testing.T) {
 	h := new(testHandler)
 	manager := NewManager(h, 10, 10*time.Second, LastItem)
 
 	for i := 0; i < 12; i++ {
-		manager.Append(fmt.Sprintf("a_%d", i))
+		item := fmt.Sprintf("a_%d", i+1)
+		manager.Append(item)
+	}
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+
+	manager.Close()
+	time.Sleep(500 * time.Microsecond)
+	assert.Equal(t, 2, h.count, fmt.Sprintf("Test failed process called more than expected 2 != %d", h.count))
+	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
+	assert.Equal(t, 2, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 2 != %d", len(h.currentData)))
+}
+
+func TestItemLimitFullLastItemWithCloseDiscard(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 10, 10*time.Second, LastItem)
+
+	for i := 0; i < 12; i++ {
+		item := fmt.Sprintf("a_%d", i+1)
+		manager.Append(item)
+	}
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+
+	manager.CloseAndDiscardRemaining()
+	time.Sleep(500 * time.Microsecond)
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
+	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+}
+
+func TestTimeLimitFullLastItem(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 20, 1*time.Second, LastItem)
+	defer manager.Close()
+
+	for i := 0; i < 12; i++ {
+		item := fmt.Sprintf("a_%d", i+1)
+		manager.Append(item)
+		time.Sleep(100 * time.Millisecond)
 	}
 
-	assert.Equal(suite.T(), 10, len(h.data), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.data)))
+	time.Sleep(500 * time.Millisecond)
+	assert.Equal(t, 0, h.count, fmt.Sprintf("Test failed process called more than expected 0 != %d", h.count))
+	assert.Equal(t, 0, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 0 != %d", len(h.totalData)))
+	assert.Equal(t, 0, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 0 != %d", len(h.currentData)))
 
+	time.Sleep(500 * time.Millisecond)
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
+	assert.Equal(t, 12, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.currentData)))
 }
