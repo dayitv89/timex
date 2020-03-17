612b1cdd49e4beb0eab744c0b0cccd18db73985a
Gaurav Sharma <gaurav.sharma@punchh.com>
2020-03-17 16:40:49 +0530

    91.9% coverage

--- timeout/timeout_test.go
+++ timeout/timeout_test.go
@@ -11,6 +11,7 @@ import (
 type testHandler struct {
 	totalData   []string
 	currentData []string
+	failedCount int
 	count       int
 }
 
@@ -26,13 +27,15 @@ func (h *testHandler) Process(d ...interface{}) error {
 		if ok {
 			h.totalData = append(h.totalData, s)
 			h.currentData = append(h.currentData, s)
+		} else {
+			return fmt.Errorf("something went wrong with data %v", d1)
 		}
 	}
 	return nil
 }
 
 func (h *testHandler) HandleProcessingError(e error) {
-	fmt.Println("some error during processing")
+	h.failedCount++
 }
 
 //First Items
@@ -48,6 +51,7 @@ func TestItemLimitFullFirstItem(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestItemLimitFullFirstItemWithClose(t *testing.T) {
@@ -61,12 +65,14 @@ func TestItemLimitFullFirstItemWithClose(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	manager.Close()
 	time.Sleep(500 * time.Millisecond)
 	assert.Equal(t, 2, h.count, fmt.Sprintf("Test failed process called more than expected 2 != %d", h.count))
 	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
 	assert.Equal(t, 2, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 2 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 func TestItemLimitFullFirstItemWithCloseDiscard(t *testing.T) {
 	h := new(testHandler)
@@ -79,12 +85,14 @@ func TestItemLimitFullFirstItemWithCloseDiscard(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	manager.CloseAndDiscardRemaining()
 	time.Sleep(500 * time.Microsecond)
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestTimeLimitFullFirstItem(t *testing.T) {
@@ -99,6 +107,7 @@ func TestTimeLimitFullFirstItem(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestTimeLimitFullFirstItemWithClose(t *testing.T) {
@@ -112,12 +121,14 @@ func TestTimeLimitFullFirstItemWithClose(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	manager.Close()
 	time.Sleep(500 * time.Microsecond)
 	assert.Equal(t, 2, h.count, fmt.Sprintf("Test failed process called more than expected 2 != %d", h.count))
 	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
 	assert.Equal(t, 2, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 2 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 // Last Items
@@ -134,6 +145,7 @@ func TestItemLimitFullLastItem(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestItemLimitFullLastItemWithClose(t *testing.T) {
@@ -147,12 +159,14 @@ func TestItemLimitFullLastItemWithClose(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	manager.Close()
 	time.Sleep(500 * time.Microsecond)
 	assert.Equal(t, 2, h.count, fmt.Sprintf("Test failed process called more than expected 2 != %d", h.count))
 	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
 	assert.Equal(t, 2, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 2 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestItemLimitFullLastItemWithCloseDiscard(t *testing.T) {
@@ -166,12 +180,14 @@ func TestItemLimitFullLastItemWithCloseDiscard(t *testing.T) {
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	manager.CloseAndDiscardRemaining()
 	time.Sleep(500 * time.Microsecond)
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 10, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.totalData)))
 	assert.Equal(t, 10, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 10 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestTimeLimitFullLastItem(t *testing.T) {
@@ -189,11 +205,13 @@ func TestTimeLimitFullLastItem(t *testing.T) {
 	assert.Equal(t, 0, h.count, fmt.Sprintf("Test failed process called more than expected 0 != %d", h.count))
 	assert.Equal(t, 0, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 0 != %d", len(h.totalData)))
 	assert.Equal(t, 0, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 0 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	time.Sleep(500 * time.Millisecond)
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
 	assert.Equal(t, 12, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 }
 
 func TestTimeLimitFullLastItemClose(t *testing.T) {
@@ -209,10 +227,26 @@ func TestTimeLimitFullLastItemClose(t *testing.T) {
 	assert.Equal(t, 0, h.count, fmt.Sprintf("Test failed process called more than expected 0 != %d", h.count))
 	assert.Equal(t, 0, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 0 != %d", len(h.totalData)))
 	assert.Equal(t, 0, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 0 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
 
 	manager.Close()
 	time.Sleep(500 * time.Millisecond)
 	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
 	assert.Equal(t, 12, len(h.totalData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.totalData)))
 	assert.Equal(t, 12, len(h.currentData), fmt.Sprintf("Test failed len is not matched to 12 != %d", len(h.currentData)))
+	assert.Equal(t, 0, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 0 != %d", h.failedCount))
+}
+
+// Processing failed
+func TestProcessingFailed(t *testing.T) {
+	h := new(testHandler)
+	manager := NewManager(h, 2, 10*time.Second, LastItem)
+	defer manager.Close()
+
+	for i := 0; i < 3; i++ {
+		manager.Append(i)
+	}
+
+	assert.Equal(t, 1, h.count, fmt.Sprintf("Test failed process called more than expected 1 != %d", h.count))
+	assert.Equal(t, 1, h.failedCount, fmt.Sprintf("Test failed count len is not matched to 1 != %d", h.failedCount))
 }
